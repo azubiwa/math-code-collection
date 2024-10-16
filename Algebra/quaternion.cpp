d33d576562f1f64c5d2672ace1e0fb6eae0dfb4a
azubiwa <103619407+azubiwa@users.noreply.github.com>
2024-10-16 16:17:51 +0900

    10-16

--- Algebra/quaternion.cpp
+++ Algebra/quaternion.cpp
@@ -6,10 +6,16 @@ std::vector<float> quat(4);
 // 四元数の足し算
 std::vector<float> sum(std::vector<float> quat_a, std::vector<float> quat_b)
 {
-    std::vector<float> return_quat;
+    std::vector<float> return_quat(4);
     for (int i = 0; i < 4; i++)
     {
         return_quat[i] = (quat_a[i] + quat_b[i]);
     }
     return return_quat;
+}
+
+// 四元数の掛け算
+std::vector<float> time(std::vector<floar> quat_a, std::vector<float> quat_b) {
+    std::vector<float return_quat(4);
+    
 }
